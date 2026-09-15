9b0084b530587ae41a6f51f4931fe8d3bb3bc60f
guilhermerlps-cpu <guilhermerlps@gmail.com>
2026-09-15 14:33:49 -0300

    Cache v13

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    sem quebrar o app já aberto: o novo só assume depois que fecha.
    ===================================================================== */
 
-const CACHE = 'gestao-rapida-manutencoes-v12';
+const CACHE = 'gestao-rapida-manutencoes-v13';
 
 const ARQUIVOS = [
   './',
